cfbddc549ca6731b4fc802c522f0729de7d62bc7
Aymen Mehri <med.aymen.mehri@gmail.com>
2016-08-15 02:04:33 +0200

    input and output properties

--- ng2-typescript-04/app/app.component.ts
+++ ng2-typescript-04/app/app.component.ts
@@ -1,7 +1,21 @@
 import {Component} from 'angular2/core';
+import {FavoriteComponent} from "./favorite.component";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>Ng Typescript 04</h1>
+        <favorite [is-favorite]="post.isFavorite" (change)="onFavoriteChange($event)"></favorite>
+        `,
+    directives : [FavoriteComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+    post = {
+        title : "Title",
+        isFavorite : true
+    }
+
+    onFavoriteChange($event){
+        console.log($event);
+    }
+}
---
+++ ng2-typescript-04/app/favorite.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, Output, EventEmitter} from "angular2/core";
+
+@Component({
+    selector : 'favorite',
+    template: `
+        <br>Exercice :<br>
+        <i 
+            class="glyphicon" 
+            [class.glyphicon-star-empty]="!isFavorite"
+            [class.glyphicon-star]="isFavorite"
+            (click)="onClick()">
+            
+        </i>
+        `
+})
+export class FavoriteComponent {
+
+    @Input('is-favorite')
+    isFavorite = false;
+
+    @Output() change = new EventEmitter();
+
+    onClick(){
+        this.isFavorite = !this.isFavorite;
+        this.change.emit({newValue : this.isFavorite});
+    }
+
+}
