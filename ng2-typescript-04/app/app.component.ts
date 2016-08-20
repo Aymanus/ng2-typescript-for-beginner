af900a3c255e9e44cae493502d8ebba06ab3d715
Aymen Mehri <med.aymen.mehri@gmail.com>
2016-08-20 23:28:23 +0200

    like exercice

--- ng2-typescript-04/app/app.component.ts
+++ ng2-typescript-04/app/app.component.ts
@@ -1,14 +1,23 @@
 import {Component} from 'angular2/core';
 import {FavoriteComponent} from "./favorite.component";
+import {LikeComponent} from "./like.component";
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Ng Typescript 04</h1>
-        <favorite [is-favorite]="post.isFavorite" (change)="onFavoriteChange($event)"></favorite>
-        <i class="glyphicon glyphicon-star"></i>
+        
+        <favorite [is-favorite]="post.isFavorite" (change)="onFavoriteChange($event)">
+        </favorite>
+        
+        <i class="glyphicon glyphicon-star">
+        </i>
+        
+        <like [totalLikes]="tweet.totalLikes" [iLike]="tweet.iLike">
+        </like>
+        
         `,
-    directives : [FavoriteComponent]
+    directives : [FavoriteComponent, LikeComponent]
 })
 export class AppComponent {
     post = {
@@ -16,7 +25,14 @@ export class AppComponent {
         isFavorite : true
     }
 
+    tweet = {
+        totalLikes : 10,
+        iLife: false
+    }
+
     onFavoriteChange($event){
         console.log($event);
     }
+
+
 }
